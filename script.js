b3b6bb0a45a285f7fc246d99bc44b6342473e1c5
PauVicto <victoria.pau@mail.utoronto.ca>
2026-03-15 19:55:39 -0400

    added mapbox token and style

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-mapbox.gl.accessToken =''
+mapbox.gl.accessToken ='pk.eyJ1IjoicGF1LXZpY3RvIiwiYSI6ImNta2Rib2s1bTA5d2MzZW9vaGF2a3hrczkifQ.ie1nrw6qR60q70TUdf5B_w'
 
 const map = new mapbox.gl.Map({
     container: 'map',
-    style: '',
+    style: 'mapbox://styles/pau-victo/cmmqya137000y01s64wtr7038',
     center: [0, 0],
     zoom: 2
 });
